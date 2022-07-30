3f24c3ffacb7008a541590e069d4a8d5b0b06e1d
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-07-30 16:27:59 +0200

    Solved. Maybe not the fastest, but easy to undestand and faster than many other's solutions.

--- 916_Word_Subsets.py
+++ 916_Word_Subsets.py
@@ -16,26 +16,40 @@ Example 1:
 
 Input: words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]
 Output: ["facebook","google","leetcode"]
+
+Result:
+Runtime: 755 ms, faster than 91.00% of Python3 online submissions for Word Subsets.
+Memory Usage: 18.6 MB, less than 54.67% of Python3 online submissions for Word Subsets.
 """
+from collections import Counter
 
 
 class Solution:
     @staticmethod
-    def wordSubsets(words1: list[str], subs: list[str]) -> list[str]:
+    def wordSubsets(words: list[str], subs: list[str]) -> list[str]:
+        # Minimize all the substrings to only one counter, which is max of all subs.
+        universal_substring_counter = Counter()
+        for substring in subs:
+            substring_counter = Counter(substring)
+            universal_substring_counter |= substring_counter
+
         out = []
-        for word in words1:
-            yep = True
-            for substring in subs:
-                if substring not in word:
-                    yep = False
-            if yep:
+        for word in words:
+            word_is_ok = True
+            for char in universal_substring_counter:
+                if universal_substring_counter[char] > word.count(char):
+                    word_is_ok = False
+                    break
+            if word_is_ok:
                 out.append(word)
-                
         return out
 
 
-words = ["amazon", "apple", "facebook", "google", "leetcode"]
+words1 = ["amazon", "apple", "facebook", "google", "leetcode"]
 words2 = ["amazon", "apple", "facebook", "google", "leetcode"]
 subs2 = ["lo", "eo"]
-assert Solution.wordSubsets(words, ["e", "o"]) == ["facebook", "google", "leetcode"]
+words3 = ["amazon", "apple", "facebook", "google", "leetcode"]
+subs3 = ["oo", "gle", "g", "gg", "e"]
+assert Solution.wordSubsets(words1, ["e", "o"]) == ["facebook", "google", "leetcode"]
 assert Solution.wordSubsets(words2, subs2) == ["google", "leetcode"]
+assert Solution.wordSubsets(words3, subs3) == ["google"]
